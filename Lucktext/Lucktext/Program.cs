2ac57e737f09530bd2cdf5236ef2348e7db6dd12
Albin Tallkvist <albin.tallkvist@elev.ga.ntig.se>
2022-08-30 10:26:46 +0200

    Initial Commit

---
+++ Lucktext/Lucktext/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+string name =  "";
+string favoriteProtein =  "";
+string pr =  "";
+
+Console.WriteLine("Vad heter du?");
+name = Console.ReadLine();
+
+Console.WriteLine("Vad är din favorit protein/supplement");
+favoriteProtein = Console.ReadLine();
+
+Console.WriteLine("Soft, vad är din pr?");
+pr = Console.ReadLine();
+
+Console.WriteLine($"Tja {name}, hörde att du bara maxar {pr}, om du ska vara så svag varför ens ta {favoriteProtein}");
+
+Console.WriteLine("");
+Console.WriteLine("Haft nog? ENTER för att ge upp");
+Console.ReadLine();
